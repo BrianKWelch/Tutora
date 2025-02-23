8dc0b782f442bf8c3f7f586247dc9c8163dcc9bf
BrianKWelch <briankwelch70@gmail.com>
2025-02-23 12:14:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Tutora Web App JavaScript
+
+console.log("Tutora Web App Loaded");
+
+// Example Event Listeners
+document.getElementById("signUpBtn").addEventListener("click", () => {
+    alert("Sign Up Clicked");
+});
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+    alert("Log In Clicked");
+});
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    alert("Log Out Clicked");
+});
